a8a84a034c228c181a47c5945b81d816990630a4
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-11-30 00:27:02 -0300

    Refactor.

--- src/Problems/CellPhoneTower/App.java
+++ src/Problems/CellPhoneTower/App.java
@@ -1,114 +1,32 @@
 package Problems.CellPhoneTower;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        int dimension = 6;
-        Item[][] map = new Item[dimension][dimension];
-        List<Item> towers = getTowers();
+        int columns = 6;
+        int rows = 6;
+        Item[][] map = new Item[rows][columns];
+        int propagation = 2;
 
-        System.out.println("Number of fields filled " + fillMap(towers, map) + " of " + (dimension * dimension));
-
-        printMap(map);
-    }
-
-    static List<Item> getTowers() {
-        List<Item> towers = new ArrayList<Item>();
-        Item item = new Item();
-        item.propagation = 2;
-        item.value = 0;
-
-        for (int i = 0; i < 6; i++) {
-            towers.add(item);
-        }
-
-        return towers;
-    }
-
-    static int fillMap(List<Item> towers, Item[][] map) {
-        int towersQuantity = 0;
-        boolean hasToJump = false;
-
-        for (Item tower : towers) {
-            for (int row = 0; row < map[0].length; row++) {
-                for (int column = 0; column < map[1].length; column++) {
-                    int leftColumnIndex = column - tower.propagation - 1;
-                    int rightColumnIndex = column + tower.propagation - 1;
-                    int aboveRowIndex = row - tower.propagation - 1;
-                    int belowRowIndex = row + tower.propagation - 1;
-
-                    if (map[row][column] == null && map[row][leftColumnIndex < 0 ? 0 : leftColumnIndex] == null
-                            && map[row][rightColumnIndex > map[1].length - 1 ? map[1].length - 1
-                                    : rightColumnIndex] == null
-                            && map[aboveRowIndex < 0 ? 0 : aboveRowIndex][column] == null
-                            && map[belowRowIndex > map[0].length - 1 ? map[0].length - 1
-                                    : belowRowIndex][column] == null) {
-                        map[row][column] = tower;
-                        towersQuantity += 1;
-
-                        if (leftColumnIndex >= 0) {
-                            System.out.println("esq:" + row + " " + column);
-                            Item item = new Item();
-                            item.value = 1;
-                            map[row][leftColumnIndex] = item;
-                            towersQuantity += 1;
-                        }
-
-                        if (rightColumnIndex <= map[1].length - 1) {
-                            System.out.println("dir:" + row + " " + column);
-                            Item item = new Item();
-                            item.value = 1;
-                            map[row][rightColumnIndex] = item;
-                            towersQuantity += 1;
-                        }
-
-                        if (aboveRowIndex >= 0) {
-                            System.out.println("cima:" + row + " " + column);
-                            Item item = new Item();
-                            item.value = 1;
-                            map[aboveRowIndex][column] = item;
-                            towersQuantity += 1;
-                        }
-
-                        if (belowRowIndex <= map[0].length - 1) {
-                            System.out.println("baixo:" + row + " " + column);
-                            Item item = new Item();
-                            item.value = 1;
-                            map[belowRowIndex][column] = item;
-                            towersQuantity += 1;
-                        }
-
-                        // hasToJump = true;
-
-                        break;
-                    } else {
-                        hasToJump = false;
-                    }
-                }
-
-                if (hasToJump) {
-                    break;
-                }
-            }
-
-            if (hasToJump) {
-                break;
+        for (int row = 0; row < rows; row += 1) {
+            for (int column = 0; column < columns; column += 1) {
+                map[row][column] = new Item();
             }
         }
 
-        return towersQuantity;
-    }
-
-    static void printMap(Item[][] map) {
-        for (int row = 0; row < map[0].length; row++) {
-            for (int column = 0; column < map[1].length; column++) {
-                System.out.print(map[row][column] != null ? map[row][column].value : "-");
-                System.out.print(" ");
+        for (int row = 0; row < rows; row += 1) {
+            for (int column = 0; column < columns; column += 1) {
+                Item current = map[row][column];
+                current.fillTower(map, propagation, column, row);
             }
+        }
 
+        for (Item[] itemMap : map) {
+            for (Item item : itemMap) {
+                System.out.print(item);
+            }
             System.out.println();
         }
+
+        System.out.println("Number of fields filled " + Item.counter + " of " + rows * columns);
     }
 }
--- src/Problems/CellPhoneTower/Item.java
+++ src/Problems/CellPhoneTower/Item.java
@@ -1,6 +1,64 @@
 package Problems.CellPhoneTower;
 
 public class Item {
-    public int propagation;
-    public int value; // tower = 0; propagation = 1
+    public int value;
+    static public int counter;
+
+    public Item() {
+        this.value = 0;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void fillTower(Item[][] map, int propagation, int column, int row) {
+        if (this.value != 0)
+            return;
+
+        for (int i = column - propagation; i < column + propagation; i += 1) {
+            for (int j = row - propagation; j < row + propagation; j += 1) {
+                if (i < 0 || j < 0 || i > 5 || j > 5)
+                    continue;
+                if (row - 1 >= 0 && map[row - 1][column].value > 0)
+                    continue;
+                if (row + 1 <= 5 && map[row + 1][column].value > 0)
+                    continue;
+                if (column - 1 >= 0 && map[row][column - 1].value > 0)
+                    continue;
+                if (column + 1 <= 5 && map[row][column + 1].value > 0)
+                    continue;
+
+                map[row][column].setValue(1);
+                counter++;
+
+                if (row - 1 >= 0) {
+                    map[row - 1][column].setValue(2);
+                    counter++;
+                }
+
+                if (row + 1 <= 5) {
+                    map[row + 1][column].setValue(2);
+                    counter++;
+                }
+
+                if (column - 1 >= 0) {
+                    map[row][column - 1].setValue(2);
+                    counter++;
+                }
+
+                if (column + 1 <= 5) {
+                    map[row][column + 1].setValue(2);
+                    counter++;
+                }                
+            }
+        }
+
+        return;
+    }
+
+    @Override
+    public String toString() {
+        return " " + this.value;
+    }
 }
